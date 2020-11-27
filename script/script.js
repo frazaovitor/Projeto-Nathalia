4b548ad71f7b034eb6337e0c1c2c61d99a8b0419
frazaovitor <51264703+frazaovitor@users.noreply.github.com>
2020-11-26 21:36:22 -0300

    arrumação das variáveis, campos de seleção dinâmicos

--- script/script.js
+++ script/script.js
@@ -1,49 +1,73 @@
 // CAPTURA OS CAMPOS DO FORMULÁRIO
 
 cadastro = document.querySelectorAll('.cadastrar')
+pecaPersonalizada = document.querySelectorAll('.pecaPersonalizada')
 
 // CAPTURA OS CAMPOS DO CONTRATO 
 
-dadosGerais = document.querySelector('.dadosGerais')
-diaCasamento = document.querySelector('.dataCasamento')
-lugarEntrega = document.querySelector('.localEntrega')
-descrever = document.querySelector('.descricao')
-diaEntrega = document.querySelector('.dataEntrega')
-totalValor = document.querySelector('.valorTotal')
-reservaValor = document.querySelector('.valorReserva')
-finalPagamento = document.querySelector('.pagamentoFinal')
+dadosContrato = document.querySelectorAll('.dadosContrato')
+
+
+// CAPTURA O CONTRATO E DO FORMULÁRIO
+
 contrato = document.querySelector('.contrato')
 formulario = document.querySelector('.formulario')
+
+
+// CAPTURA O BOTÃO
 botao = document.querySelector('.enviar')
 
 botao.addEventListener('click', () => {
 
     // VERIFICA SE EXISTE ALGUM CAMPO NÃO PREENCHIDO
 
-    if (cadastro[0].value != '' && cadastro[1].value != '' && cadastro[2].value != '' && cadastro[3].value != '' && cadastro[4].value != '' && cadastro[5].value != '' && cadastro[6].value != '' && cadastro[7].value != '' && cadastro[8].value != '' && cadastro[9].value != '' && cadastro[10].value != '' && cadastro[11].value != '' && cadastro[12].value != '' && cadastro[13].value != '' && cadastro[14].value != '' && cadastro[15].value != '' && cadastro[16] != '' && cadastro[17] != '') {
+    if (true) {
 
         // ATUALIZA O CONTRATO COM OS VALORES DIGITADOS E FORMATA OS CAMPOS
 
-        dadosGerais.innerHTML = `Contratante: ${nome.value}. Profissão: ${profissao.value}. Rg:
-    ${rg.value.replace(/(\d{1,2})(\d{3})(\d{3})(\d{1})$/, "$1.$2.$3-$4")}. CPF: ${cpf.value.replace(/(\d{3})?(\d{3})?(\d{3})?(\d{2})/, "$1.$2.$3-$4")}. CEP: ${cep.value.replace(/(\d{5})(\d{3})$/, "$1-$2")}. Endereço completo: ${endereco.value}. Telefone para contato: ${tel.value.replace(/(\d{2})(\d{5})(\d{4})$/, "($1) $2-$3")}. E-mail: ${email.value}.`
+        dadosGerais.innerHTML =
+            `Contratante: ${nome.value}.
+          Profissão: ${profissao.value}. 
+          Rg: ${rg.value.replace(/(\d{1,2})(\d{3})(\d{3})(\d{1})$/, "$1.$2.$3-$4")}. 
+          CPF: ${cpf.value.replace(/(\d{3})?(\d{3})?(\d{3})?(\d{2})/, "$1.$2.$3-$4")}. 
+          CEP: ${cep.value.replace(/(\d{5})(\d{3})$/, "$1-$2")}. 
+          Endereço completo: ${endereco.value}. 
+          Telefone para contato: ${tel.value.replace(/(\d{2})(\d{5})(\d{4})$/, "($1) $2-$3")}. 
+          E-mail: ${email.value}.`
 
-        diaCasamento.innerHTML = `Data do casamento: ${dataCasamento.value.split('-').reverse().join('/')}`
-        diaEntrega.innerHTML = ` Data de entrega da peça: ${dataEntrega.value.split('-').reverse().join('/')}`
-        descrever.innerHTML = `Descrição técnica da peça: ${descricao.value}, Entremeio: ${entremeio.value} (Código: ${entremeioCod.value})`
-        totalValor.innerHTML = `Valor: O valor total para a criação/confecção da peça escolhida é de R$ ${valorTotal.value}`
-        reservaValor.innerHTML = `Valor de R$ ${valorReserva.value} efetuado no dia ${diaReserva.value.split('-').reverse().join('/')}  para a
+        dataCasamentoContrato.innerHTML = `Data do casamento: ${formatData(dataCasamento.value)}`
+        dataEntregaContrato.innerHTML = ` Data de entrega da peça: ${formatData(dataEntrega.value)}`
+        valorTotalContrato.innerHTML = `Valor: O valor total para a criação/confecção da peça escolhida é de R$ ${valorTotal.value}`
+        valorReservaContrato.innerHTML = `Valor de R$ ${valorReserva.value} efetuado no dia ${formatData(diaReserva.value)}  para a
         reserva de sua peça.`
-        finalPagamento.innerHTML = `Dia ${pagamentoFinal.value.split('-').reverse().join('/')} efetivar o pagamento de 50% do valor da criação da peça e
-        os outros 50% em dinheiro no dia da entrega (${dataEntrega.value.split('-').reverse().join('/')})`
+
+        switch (descricaoPeca.value) {
+            case "pecaFechada":
+                descricaoTecnica.innerHTML = `Descrição Técnica da Peça: Peça fechada (Código: ${pecaFechadaCod.value})`
+                break;
+            case "pecaPersonalizada":
+                descricaoTecnica.innerHTML = `Descrição Técnica da Peça: Peça Personalizada Pedraria: ${pedraria.value} (Código: ${pedrariaCod.value}), Cruz: ${modeloCruz.value} (Código: ${modeloCruzCod.value}), Entremeio: ${entremeio.value} (Código: ${entremeioCod.value}).`
+
+                break;
+
+            default:
+                break;
+        }
+
+        // LOCAL DE ENTREGA E FINALIZAÇÃO DO PAGAMENTO 
 
         switch (localEntrega.value) {
             case "novaAmerica":
-                lugarEntrega.innerHTML = `Local da entrega/retirada da peça: Entrega será realizada no Shopping Nova América no dia ${dataEntrega.value.split('-').reverse().join('/')} na parte da manhã.`
+                localEntregaContrato.innerHTML = `Local da entrega/retirada da peça: Entrega será realizada no Shopping Nova América no dia ${formatData(dataEntrega.value)} na parte da manhã.`
+                pagamentoFinalContrato.innerHTML = `Dia ${formatData(pagamentoFinal.value)} efetivar o pagamento de 50% do valor da criação da peça e
+        os outros 50% em dinheiro no dia da entrega (${formatData(dataEntrega.value)})`
+
                 break;
+
             case "correio":
-                lugarEntrega.innerHTML = `A entrega será efetuala pelo correio.`
-                break;
-            default:
+                localEntregaContrato.innerHTML = `A entrega será efetuala pelo correio.`
+                pagamentoFinalContrato.innerHTML = `Dia ${formatData(pagamentoFinal.value)} efetivar o pagamento de 100% do valor da criação da peça mais o acréscimo do frete.`
+
                 break;
         }
         // RETIRA O CAMPO DE FORMULÁRIO, O BOTÃO E FAZ APARECER O CONTRATO PRONTO! 
@@ -57,26 +81,66 @@ botao.addEventListener('click', () => {
     }
 })
 
+// CALLBACK PARA COMPLETAR O CEP
 function meu_callback(conteudo) {
     if (!("erro" in conteudo)) {
         // ATUALIZA O FORMULÁRIO ASSIM QUE COLOCA O CEP.
-        endereco.value = `${conteudo.logradouro}, Bairro: ${conteudo.bairro}, Cidade: ${conteudo.localidade}, UF: ${conteudo.uf}`
-        // endereco.value = enderecoCompleto
+        endereco.value = `${conteudo.logradouro}, ${numero.value}, Complemento: ${complemento.value}, Bairro: ${conteudo.bairro}, Cidade: ${conteudo.localidade}/${conteudo.uf}`
     } else {
-        //CEP não Encontrado.
-        alert("CEP não encontrado.");
+        alert("CEP não encontrado. Digite o endereço a mão ou verifique se o CEP existe neste site: http://www.buscacep.correios.com.br/sistemas/buscacep/BuscaCepEndereco.cfm");
     }
 }
 
+// FAZ PESQUISA DO CEP
 function pesquisaCep(valor) {
-
     let script = document.createElement('script');
 
     //Sincroniza com o callback.
-    script.src = 'https://viacep.com.br/ws/' + cep.value + '/json/?callback=meu_callback';
+    script.src = 'https://viacep.com.br/ws/' + valor + '/json/?callback=meu_callback';
 
     //Insere script no documento e carrega o conteúdo.
     document.body.appendChild(script);
-    console.log(script)
 }
 
+// FORMATA AS DATAS PARA DD/MM/AAAA
+function formatData(data) {
+    return data.split('-').reverse().join('/')
+}
+
+// VERIFICA SE EXISTE ALGUM CAMPO NÃO PREENCHIDO
+function verificaVazio(cadastro) {
+    for (i = 0; i < cadastro.length; i++) {
+        if (cadastro[i].value != '') {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+// APAGA OS CAMPOS NÃO SELECIONADOS (PEÇA FECHADA OU PEÇA PERSONALIZADA)
+function selecaoPeca() {
+    if (descricaoPeca.value == "pecaFechada") {
+        entremeio.style = 'display: none'
+        entremeioCod.style = 'display: none'
+        entremeioL.style = 'display: none'
+        entremeioCodL.style = 'display: none'
+        modeloCruz.style = 'display: none'
+        modeloCruzL.style = 'display: none'
+        modeloCruzCod.style = 'display: none'
+        modeloCruzCodL.style = 'display: none'
+        pedraria.style = 'display: none'
+        pedrariaL.style = 'display: none'
+    } else {
+        entremeio.style = 'display: block'
+        entremeioCod.style = 'display: block'
+        entremeioL.style = 'display: block'
+        entremeioCodL.style = 'display: block'
+        modeloCruz.style = 'display: block'
+        modeloCruzL.style = 'display: block'
+        modeloCruzCod.style = 'display: block'
+        modeloCruzCodL.style = 'display: block'
+        pedraria.style = 'display: block'
+        pedrariaL.style = 'display: block'
+    }
+}
